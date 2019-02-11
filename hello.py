1f7f068e09577a368b4957b963c13a4e5b353975
DrFelixC <amberchin0215@gmail.com>
2019-02-11 14:33:43 +0800

    add a file

---
+++ hello.py
@@ -0,0 +1,13 @@
+Python 3.6.4 (v3.6.4:d48ecebad5, Dec 18 2017, 21:07:28) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+hello
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    hello
+NameError: name 'hello' is not defined
+>>> print("hello")
+hello
+>>> 
